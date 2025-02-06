fdc0ca9e2dc4b353b6ccb33806a2b3719cabd2c6
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-02-06 13:37:49 +0400

    deleted unnecesary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,21 +7,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrderModule } from './order/order.module';
-import { CartModule } from './cart/cart.module';
-import { OrdersModule } from './orders/orders.module';
-import { WishlistModule } from './wishlist/wishlist.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
-    
+
     UsersModule,
     ProductsModule,
     OrderModule,
-    CartModule,
-    OrdersModule,
-    WishlistModule,
   ],
   controllers: [AppController],
   providers: [AppService],
